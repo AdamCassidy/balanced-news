1548758b372106036eaf49b202dc5ab13a3475d5
Adam Cassidy <adamcassidy011@gmail.com>
2020-12-19 21:54:10 -0500

    Add styling and unique id generator
    To make images smaller

--- src/Articles.tsx
+++ src/Articles.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Article from './Article';
 import type { ArticleProps} from './Article';
-import nextId from "react-id-generator";
+import nextId from 'react-id-generator';
 
 interface Props { 
     articles: ArticleProps[];
@@ -10,7 +10,7 @@ interface Props {
 const Articles: React.FC<Props> = ({articles}): JSX.Element => {
     return (
         <ul>
-            {articles.map(article => <li><Article key={article.toString()} url={article.url} title={article.title} urlToImage={article.urlToImage}></Article></li>)}
+            {articles.map((article) => <li key={nextId()} ><Article url={article.url} title={article.title} urlToImage={article.urlToImage}></Article></li>)}
         </ul>        
     );
 }
