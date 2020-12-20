c8d3ec9609979a885f083998dd28fafc594874d0
Adam Cassidy <adamcassidy011@gmail.com>
2020-12-20 10:58:55 -0500

    Add empty search option for good/bad news
    So that the ratio is taken into account when the search is empty.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import { getArticles } from "./newsApi";
-import type {ArticleProps} from "./Article";
+import type { ArticleProps } from "./Article";
 import Articles from "./Articles";
 
 function App(): JSX.Element {
@@ -20,12 +20,53 @@ function App(): JSX.Element {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(articles));
   }, [articles]);
 
+  const generateSearch = async (ratio: number) => {
+    let negativeWords = [""];
+    let positiveWords = [""];
+    let search = "";
+    let count = 0;
+
+    const negRes = await fetch("negative-words.txt");
+    const negText = await negRes.text();
+    negativeWords = negText.toString().split("\n");
+
+    const posRes = await fetch("positive-words.txt");
+    const posText = await posRes.text();
+    positiveWords = posText.toString().split("\n");
+    
+    // Two words is a good trade-off between amount of articles and good/bad accuracy
+    while (count < 2) {
+      if (Math.random() * 100 < ratio) {
+        if (positiveWords.length) {
+          search +=
+            positiveWords[
+              Math.floor(Math.random() * positiveWords.length - 1)
+            ] + " ";
+        }
+      } else {
+        if (negativeWords.length) {
+          search +=
+            negativeWords[
+              Math.floor(Math.random() * negativeWords.length - 1)
+            ] + " ";
+        }
+      }
+      count += 1;
+    }
+    return search;
+  };
   const getNews = async () => {
     if (searchRef && searchRef.current) {
-      const search = searchRef.current.value.toString() || "";
+      let search = searchRef.current.value.toString() || "";
       if (ratioRef && ratioRef.current) {
-        const ratio = parseFloat(ratioRef.current.value.toString()) || 50;
-        const data: ArticleProps[] = await getArticles(search, ratio)
+        let ratio = parseFloat(ratioRef.current.value.toString()) || 50;
+        if (ratio > 100) { ratio = 100} 
+        else if (ratio < 0) {ratio = 0}
+        if (search === "") {
+          search = await generateSearch(ratio);
+          console.log(search);
+        }
+        const data: ArticleProps[] = await getArticles(search);
         setArticles(data);
       }
     }
@@ -34,9 +75,9 @@ function App(): JSX.Element {
   return (
     <>
       <input type="text" ref={searchRef} placeholder="Search" />
-      <input type="number" ref={ratioRef} placeholder="Ratio" />
+      <input type="number" ref={ratioRef} placeholder="Good news probability (out of 100)" style={{ width:"300px" }}/>
       <button onClick={getNews}>Search</button>
-      <Articles articles={articles}></Articles>
+      {articles ? <Articles articles={articles}></Articles> : null}
     </>
   );
 }
