bfd67d3f5739eb7808d85ff6a54679df497e8db6
Adam Cassidy <adamcassidy011@gmail.com>
2020-12-25 16:47:32 -0500

    Make articles a set
    To make sure there's no complete duplicate articles, but there are still
    duplicates because of the news api.

--- src/newsApi.tsx
+++ src/newsApi.tsx
@@ -1,15 +1,15 @@
 import axios from "axios";
+import { ArticleProps } from "./Article";
 
 const baseUrl = "https://newsapi.org/v2/";
 const everythingUrl = baseUrl + "everything/";
 
 export const getArticles = async (search: string) => {
-  let url = everythingUrl  + "?language=en&pageSize=100";
+  let url = everythingUrl + "?language=en&pageSize=100";
   if (search !== "") {
-    url +="&q=" + search;
+    url += "&q=" + search;
   }
   url += "&apiKey=" + process.env.REACT_APP_API_KEY;
-
   const articles = await axios
     .get(url)
     .then((data) => data.data.articles)
@@ -17,5 +17,6 @@ export const getArticles = async (search: string) => {
       console.log(err);
       return;
     });
-  return articles;
+
+  return new Array<ArticleProps>(...new Set<ArticleProps>(articles));
 };
