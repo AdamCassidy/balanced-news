fe55e131c9c18507b32329042a3662c839f7c6ea
Adam Cassidy <adamcassidy011@gmail.com>
2021-01-09 16:09:47 -0500

    Hot load during dev and remove horizontal padding
    Removed horizontal padding for articles

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { hot } from "react-hot-loader";
 
-ReactDOM.render(
+const WrappedApp = ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
+export default process.env.NODE_ENV === "development"
+  ? hot(module)(WrappedApp)
+  : WrappedApp;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
