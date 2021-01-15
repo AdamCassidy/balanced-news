0f097125448db987a69a82bb4ebcc5408653e1b3
Adam Cassidy <adamcassidy011@gmail.com>
2021-01-15 11:46:24 -0500

    Add firebase auth for signup
    No validation yet.

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useContext } from "react";
+import { useEffect } from "react";
+import { auth } from "../firebase";
+import firebase from "firebase";
+
+interface ContextProps {
+  currentUser?: firebase.User | null;
+  signup?: (
+    email: string,
+    password: string
+  ) => Promise<firebase.auth.UserCredential>;
+}
+
+const AuthContext: React.Context<ContextProps> = React.createContext({});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
+
+  const signup = (email: string, password: string) => {
+    return auth.createUserWithEmailAndPassword(email, password);
+  };
+  useEffect(() => {
+    const unsuscribe = auth.onAuthStateChanged((user) => {
+      if (user) setCurrentUser(user);
+    });
+    return unsuscribe;
+  }, []);
+
+  const value = { signup, currentUser };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
