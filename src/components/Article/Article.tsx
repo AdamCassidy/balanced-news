3a22a74c301fca4757c634b17d09f38f7856bf81
Adam Cassidy <adamcassidy011@gmail.com>
2021-01-24 17:53:27 -0500

    Fix so you can't click article margins
    Link only covers the actual card now and not the grid item which is
    bigger than the card.

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -27,24 +27,26 @@ const Article: React.FC<ArticleProps> = ({
   return (
     <Grid container justify="center" alignItems="center">
       <Grid item>
-        <Link
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ textDecoration: "none" }}
-        >
-          <Card className="article-card">
-            <CardContent>
-              <Typography variant="h5" align="center" className="card-title">
-                {title}
-              </Typography>
-              <Typography align="center">{description}</Typography>
-            </CardContent>
-            <CardMedia>
-              <img src={urlToImage} alt="" className="article-image center" />
-            </CardMedia>
-          </Card>
-        </Link>
+        <div className="article-div">
+          <Link
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            <Card className="article-card">
+              <CardContent>
+                <Typography variant="h5" align="center" className="card-title">
+                  {title}
+                </Typography>
+                <Typography align="center">{description}</Typography>
+              </CardContent>
+              <CardMedia>
+                <img src={urlToImage} alt="" className="article-image center" />
+              </CardMedia>
+            </Card>
+          </Link>
+        </div>
       </Grid>
     </Grid>
   );
