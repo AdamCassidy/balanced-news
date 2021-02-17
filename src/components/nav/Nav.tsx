f18daf4552d88332822eb6002449b7421a989e9f
Adam Cassidy <adamcassidy011@gmail.com>
2021-02-16 23:58:30 -0500

    Remove placeholder drawer links

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -148,17 +148,6 @@ function ResponsiveDrawer(props: Props) {
           </Link>
         ))}
       </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
     </div>
   );
 
